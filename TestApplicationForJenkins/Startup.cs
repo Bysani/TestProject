257b86909c88d5686be3f4eb6bf588b951d63f92
chaithanya <bchaithanya@opteamix.com>
2018-06-14 10:48:37 +0530

    First Commit

---
+++ TestApplicationForJenkins/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestApplicationForJenkins.Startup))]
+namespace TestApplicationForJenkins
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
