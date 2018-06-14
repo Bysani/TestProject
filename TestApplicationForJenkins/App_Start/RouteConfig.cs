9abf9c9c8a88a01eb7b8ba06ad91cd26c7047d92
chaithanya <bchaithanya@opteamix.com>
2018-06-14 17:42:49 +0530

    added new page

--- TestApplicationForJenkins/App_Start/RouteConfig.cs
+++ TestApplicationForJenkins/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TestApplicationForJenkins
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Test", action = "LoadTestPage", id = UrlParameter.Optional }
             );
         }
     }
